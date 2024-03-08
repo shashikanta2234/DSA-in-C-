2590f10b0c082cc72aa4389aee76657499f2d34a
Shashikanta Mahato <shashikanta2234@gmail.com>
2024-03-09 00:00:35 +0530

    day 7 repush

--- day6/hashing.cpp
+++ day6/hashing.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
-
+//find the frequency of the element in an int type array.
 int main(){
     //Array
     int a[]={2,5,9,3,7,6,4,2,6,1,8};
     //Hash Array initialisation with values 0
     int hash[12]={0};
-    for (int i=0;i<12;i++) {
+    for (int i=0;i<10;i++) {
         hash[a[i]]++;
         }
     
     cout<<"The frequency distribution of the array elements is : \n";
-    for(int j = 0 ;j < 12 ; j++ ) {
+    for(int j = 0 ;j < 10 ; j++ ) {
         if(hash[j] !=  0)
             cout << "Element " << j << " occurs " << hash[j] << " times\n";
     }
